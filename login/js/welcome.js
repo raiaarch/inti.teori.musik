eb5f6a8bec837090968447d0207f470ac58a54c9
raiaarch <alan.raia.kartiko@gmail.com>
2026-05-24 13:17:48 +0700

    Update welcome.js

--- login/js/welcome.js
+++ login/js/welcome.js
@@ -1,25 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
-
     function goLogin() {
         window.location.href = "login/index.html";
     }
-
     function logout() {
         localStorage.removeItem("username");
         location.reload();
     }
-
     const user = localStorage.getItem("username");
-
     if (user) {
+        // Desktop
         document.getElementById("userInfo").innerText = "Halo, " + user;
-
         document.getElementById("authArea").innerHTML = `
             <button onclick="logout()" class="nav-cta">Logout</button>
         `;
+        // Mobile
+        document.getElementById("mobileUserInfo").innerText = "Halo, " + user;
+        document.getElementById("mobileAuthArea").innerHTML = `
+            <button onclick="logout()" class="nav-cta">Logout</button>
+        `;
     }
-
     window.goLogin = goLogin;
     window.logout = logout;
-
 });
